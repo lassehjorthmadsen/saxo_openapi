42de7c609587d9ecbfce88275058908a51d352a1
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2022-12-17 00:15:37 +0100

    more examples

--- example.py
+++ example.py
@@ -1,13 +1,18 @@
 from saxo_openapi import API
 import saxo_openapi.endpoints.rootservices as rs
-from pprint import pprint
+import saxo_openapi.endpoints.portfolio as pf
 
-with open('C:/Users/LMDN/token24.txt') as f:
+from pprint import pprint
+from os import path
+
+home_directory = path.expanduser( '~' )
+token_directory = home_directory + '\\token24.txt'
+
+with open(token_directory) as f:
     token24 = f.readlines()
 
 token24 = token24[0]
 
-# token = "eyJhbGciOiJFUzI1NiIsIng1dCI6IkRFNDc0QUQ1Q0NGRUFFRTlDRThCRDQ3ODlFRTZDOTEyRjVCM0UzOTQifQ.eyJvYWEiOiI3Nzc3NSIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiY2lkIjoibU0zV1o1YU1WTXwyZ201Zk95ckxrdz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiZWUxZDY3NmU2MmM4NDY0NWFlNzg3ZGYxYzViNjZiOGYiLCJkZ2kiOiI4NCIsImV4cCI6IjE2NzExNzczMzciLCJvYWwiOiIxRiIsImlpZCI6ImU2NjUxMjhjMzNjZTQyNDQzNjgxMDhkYWJkODQ5NjQ1In0.6hnv_sgxP4oLbO0siXZ25NFeAV7DT-0dHAkgvXr7jRgzpiuYdjxJnWNjktxShbHxcvbo_3hHTD-a8WkmsjH-wA"
 client = API(access_token=token24)
 
 # lets make a diagnostics request, it should return '' with a state 200
@@ -24,3 +29,18 @@ print("request is: ", r)
 print("response: ")
 pprint(rv, indent=2)
 print(r.status_code)
+
+# request client details
+r = pf.clients.ClientDetailsMe()
+rv = client.request(r)
+pprint(rv)
+
+# params from above
+params = {
+    'ClientKey': rv['ClientKey']
+}
+
+# request the balances
+r = pf.balances.AccountBalances(params=params)
+rv = client.request(r)
+pprint(rv)
